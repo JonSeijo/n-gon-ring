bb2470c2143025ecf4afee33a10dfdb8ec9827ec
jonseijo <jon.seijo@gmail.com>
2016-12-30 23:47:06 -0300

    Display de valor de nodo con font default

--- core/src/com/jashlaviu/ngon/Nodo.java
+++ core/src/com/jashlaviu/ngon/Nodo.java
@@ -2,18 +2,23 @@ package com.jashlaviu.ngon;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Nodo {
     private Texture texture;
     private int value;
+    private BitmapFont font;
 
-    public Nodo() {
+    public Nodo(int value) {
         this.texture = new Texture(Gdx.files.internal("cell_blue.png"));
+        this.value = value;
+        this.font = new BitmapFont();
     }
 
     public void render(SpriteBatch batch, Pos pos) {
         batch.draw(this.texture, pos.x, pos.y);
+        this.font.draw(batch, String.valueOf(this.value), pos.x + 12, pos.y + 22);
     }
 
     public void setValue(int value) {
@@ -26,6 +31,7 @@ public class Nodo {
 
     public void dispose() {
         this.texture.dispose();
+        this.font.dispose();
     }
 
 }
