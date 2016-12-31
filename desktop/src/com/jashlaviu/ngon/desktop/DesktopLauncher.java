3bc9add9170bbacb801d5bf94e7129be5787a801
jonseijo <jon.seijo@gmail.com>
2016-12-30 22:46:54 -0300

    Initial commit

---
+++ desktop/src/com/jashlaviu/ngon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jashlaviu.ngon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jashlaviu.ngon.NgonRingGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NgonRingGame(), config);
+	}
+}
