efd48257f6c54db90d9232f5e9535c7376514c57
jonseijo <jon.seijo@gmail.com>
2016-12-30 22:53:43 -0300

    Remove image and use camera

--- core/src/com/jashlaviu/ngon/NgonRingGame.java
+++ core/src/com/jashlaviu/ngon/NgonRingGame.java
@@ -3,17 +3,23 @@ package com.jashlaviu.ngon;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class NgonRingGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private SpriteBatch batch;
+	private Viewport viewport;
+	private OrthographicCamera camera;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		this.batch = new SpriteBatch();
+        this.camera = new OrthographicCamera();
+        this.viewport = new ExtendViewport(400, 600, this.camera);
+        this.camera.position.set(200, 300, 0);
 	}
 
 	@Override
@@ -21,13 +27,12 @@ public class NgonRingGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		// batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
