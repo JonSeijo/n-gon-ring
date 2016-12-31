b1e154ed6f46104e663b11fc2902c83c17598d67
jonseijo <jon.seijo@gmail.com>
2016-12-30 23:04:24 -0300

    Use node class in game

--- core/src/com/jashlaviu/ngon/NgonRingGame.java
+++ core/src/com/jashlaviu/ngon/NgonRingGame.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -14,12 +13,16 @@ public class NgonRingGame extends ApplicationAdapter {
 	private Viewport viewport;
 	private OrthographicCamera camera;
 
+	private Nodo nodoTest;
+
 	@Override
 	public void create () {
 		this.batch = new SpriteBatch();
         this.camera = new OrthographicCamera();
         this.viewport = new ExtendViewport(400, 600, this.camera);
         this.camera.position.set(200, 300, 0);
+
+        this.nodoTest = new Nodo();
 	}
 
 	@Override
@@ -27,7 +30,7 @@ public class NgonRingGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		// batch.draw(img, 0, 0);
+		this.nodoTest.render(batch, 0, 0);
 		batch.end();
 	}
 	
